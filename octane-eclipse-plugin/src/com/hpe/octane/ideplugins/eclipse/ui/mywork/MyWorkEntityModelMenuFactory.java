55b3623bbd14eb5ddd82e9ed26a62d14e0863d11
Andras Toth <atoth@hpe.com>
2017-04-15 18:30:26 +0300

    Added option to open entity in the built in Eclipse browser

--- octane-eclipse-plugin/src/com/hpe/octane/ideplugins/eclipse/ui/mywork/MyWorkEntityModelMenuFactory.java
+++ octane-eclipse-plugin/src/com/hpe/octane/ideplugins/eclipse/ui/mywork/MyWorkEntityModelMenuFactory.java
@@ -2,6 +2,9 @@ package com.hpe.octane.ideplugins.eclipse.ui.mywork;
 
 import static com.hpe.adm.octane.services.util.Util.getUiDataFromModel;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -10,14 +13,18 @@ import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
 
 import com.hpe.adm.nga.sdk.model.EntityModel;
+import com.hpe.adm.nga.sdk.model.ReferenceFieldModel;
 import com.hpe.adm.octane.services.EntityService;
 import com.hpe.adm.octane.services.filtering.Entity;
 import com.hpe.adm.octane.services.mywork.MyWorkService;
 import com.hpe.adm.octane.services.mywork.MyWorkUtil;
+import com.hpe.adm.octane.services.util.UrlParser;
 import com.hpe.adm.octane.services.util.Util;
+import com.hpe.octane.ideplugins.eclipse.Activator;
 import com.hpe.octane.ideplugins.eclipse.ui.editor.EntityModelEditor;
 import com.hpe.octane.ideplugins.eclipse.ui.editor.EntityModelEditorInput;
 import com.hpe.octane.ideplugins.eclipse.ui.entitylist.EntityModelMenuFactory;
@@ -62,9 +69,34 @@ public class MyWorkEntityModelMenuFactory implements EntityModelMenuFactory {
 
         addMenuItem(
                 menu,
-                "View in browser",
+                "View in browser (System)",
                 ImageResources.BROWSER_16X16.getImage(),
                 () -> entityService.openInBrowser(entityModel));
+
+        addMenuItem(
+                menu,
+                "View in browser (Eclipse)",
+                ImageResources.BROWSER_16X16.getImage(),
+                () -> {
+                    Entity ownerEntityType = null;
+                    Integer ownerEntityId = null;
+                    if (entityType == Entity.COMMENT) {
+                        ReferenceFieldModel owner = (ReferenceFieldModel) Util.getContainerItemForCommentModel(entityModel);
+                        ownerEntityType = Entity.getEntityType(owner.getValue());
+                        ownerEntityId = Integer.valueOf(Util.getUiDataFromModel(owner, "id"));
+                    }
+                    URI uri = UrlParser.createEntityWebURI(
+                            Activator.getConnectionSettings(),
+                            entityType == Entity.COMMENT ? ownerEntityType : entityType,
+                            entityType == Entity.COMMENT ? ownerEntityId : entityId);
+                    try {
+                        PlatformUI.getWorkbench().getBrowserSupport().createBrowser(uri.toString()).openURL((uri.toURL()));
+                    } catch (PartInitException | MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                });
+
+        new MenuItem(menu, SWT.SEPARATOR);
 
         if (entityType != Entity.COMMENT) {
             addMenuItem(
@@ -109,13 +141,7 @@ public class MyWorkEntityModelMenuFactory implements EntityModelMenuFactory {
                 entityType == Entity.QUALITY_STORY ||
                 entityType == Entity.TASK) {
 
-            addMenuItem(
-                    menu,
-                    "Stop work",
-                    ImageResources.STOP_TIMER_16X16.getImage(),
-                    () -> {
-                        System.out.println("Please imlement me");
-                    });
+            new MenuItem(menu, SWT.SEPARATOR);
 
             addMenuItem(
                     menu,
@@ -124,9 +150,18 @@ public class MyWorkEntityModelMenuFactory implements EntityModelMenuFactory {
                     () -> {
                         System.out.println("Please imlement me");
                     });
+
+            addMenuItem(
+                    menu,
+                    "Stop work",
+                    ImageResources.STOP_TIMER_16X16.getImage(),
+                    () -> {
+                        System.out.println("Please imlement me");
+                    }).setEnabled(false);
         }
 
         if (myWorkService.isAddingToMyWorkSupported(entityType) && MyWorkUtil.isUserItemDismissible(userItem)) {
+            new MenuItem(menu, SWT.SEPARATOR);
             addMenuItem(
                     menu,
                     "Dismiss",
@@ -139,7 +174,7 @@ public class MyWorkEntityModelMenuFactory implements EntityModelMenuFactory {
         return menu;
     }
 
-    private void addMenuItem(Menu menu, String text, Image image, Runnable selectAction) {
+    private MenuItem addMenuItem(Menu menu, String text, Image image, Runnable selectAction) {
         MenuItem menuItem = new MenuItem(menu, SWT.NONE);
         if (image != null) {
             menuItem.setImage(image);
@@ -151,6 +186,7 @@ public class MyWorkEntityModelMenuFactory implements EntityModelMenuFactory {
                 selectAction.run();
             }
         });
+        return menuItem;
     }
 
 }
