3f1650502d2f58768998b19870659a0973e13062
Andras Toth <atoth@hpe.com>
2017-04-20 12:05:04 +0300

    Changed list selection to system default, Konami code will now make the list selection pink (as it should be)

--- octane-eclipse-plugin/src/com/hpe/octane/ideplugins/eclipse/ui/entitylist/custom/FatlineEntityListViewer.java
+++ octane-eclipse-plugin/src/com/hpe/octane/ideplugins/eclipse/ui/entitylist/custom/FatlineEntityListViewer.java
@@ -27,12 +27,19 @@ import com.hpe.adm.nga.sdk.model.EntityModel;
 import com.hpe.octane.ideplugins.eclipse.ui.entitylist.EntityListViewer;
 import com.hpe.octane.ideplugins.eclipse.ui.entitylist.EntityModelMenuFactory;
 import com.hpe.octane.ideplugins.eclipse.ui.entitylist.EntityMouseListener;
+import com.hpe.octane.ideplugins.eclipse.ui.util.KonamiCodeListener;
 import com.hpe.octane.ideplugins.eclipse.util.resource.SWTResourceManager;
 
 public class FatlineEntityListViewer extends Composite implements EntityListViewer {
 
     private EntityModelRenderer entityModelRenderer;
-    private static final Color selectionBackgroundColor = SWTResourceManager.getColor(255, 105, 180);
+
+    private static Color selectionBackgroundColor = SWTResourceManager.getColor((SWT.COLOR_LIST_SELECTION));
+    private static Runnable makeSelectioPinkRunnable = () -> {
+        selectionBackgroundColor = SWTResourceManager.getColor(255, 105, 180);
+    };
+    private static KonamiCodeListener konamiCodeListener;
+
     private static final Color selectionForegroundColor = SWTResourceManager.getColor(255, 255, 255);
     private static final Color backgroundColor = SWTResourceManager.getColor(255, 255, 255);
     private static final Color foregroundColor = SWTResourceManager.getColor(0, 0, 0);
@@ -67,8 +74,13 @@ public class FatlineEntityListViewer extends Composite implements EntityListView
             int style,
             EntityModelMenuFactory entityModelMenuFactory,
             EntityModelRenderer entityModelRenderer) {
-
         super(parent, SWT.NONE);
+
+        if (konamiCodeListener == null) {
+            konamiCodeListener = new KonamiCodeListener(makeSelectioPinkRunnable);
+            parent.getDisplay().addFilter(SWT.KeyDown, konamiCodeListener);
+        }
+
         setLayout(new FillLayout(SWT.HORIZONTAL));
 
         this.entityModelRenderer = entityModelRenderer;
