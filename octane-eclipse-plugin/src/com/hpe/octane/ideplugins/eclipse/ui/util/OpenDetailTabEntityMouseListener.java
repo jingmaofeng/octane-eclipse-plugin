c418faf6ab49d0328eb88eefde5d669234190bfd
Andras Toth <atoth@hpe.com>
2017-09-19 10:58:31 +0300

    comment click fix

--- octane-eclipse-plugin/src/com/hpe/octane/ideplugins/eclipse/ui/util/OpenDetailTabEntityMouseListener.java
+++ octane-eclipse-plugin/src/com/hpe/octane/ideplugins/eclipse/ui/util/OpenDetailTabEntityMouseListener.java
@@ -33,35 +33,34 @@ import com.hpe.octane.ideplugins.eclipse.ui.entitylist.EntityMouseListener;
 
 public class OpenDetailTabEntityMouseListener implements EntityMouseListener {
 
-    private static final ILog logger = Activator.getDefault().getLog();
+	private static final ILog logger = Activator.getDefault().getLog();
 
-    @Override
-    public void mouseClick(EntityModel entityModel, MouseEvent event) {
-        // Open detail tab
-        if (event.count == 2) {
-            IWorkbench wb = PlatformUI.getWorkbench();
-            IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
-            IWorkbenchPage page = win.getActivePage();
+	@Override
+	public void mouseClick(EntityModel entityModel, MouseEvent event) {
+		// Open detail tab
+		if (event.count == 2) {
+			IWorkbench wb = PlatformUI.getWorkbench();
+			IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
+			IWorkbenchPage page = win.getActivePage();
 
-            if (Entity.USER_ITEM == Entity.getEntityType(entityModel)) {
-                entityModel = MyWorkUtil.getEntityModelFromUserItem(entityModel);
-            }
+			if (Entity.USER_ITEM == Entity.getEntityType(entityModel)) {
+				entityModel = MyWorkUtil.getEntityModelFromUserItem(entityModel);
+			}
 
-            if (Entity.COMMENT == Entity.getEntityType(entityModel)) {
-                entityModel = (EntityModel) Util.getContainerItemForCommentModel(entityModel).getValue();
-            }
-            else {
-                Long id = Long.parseLong(entityModel.getValue("id").getValue().toString());
-                EntityModelEditorInput entityModelEditorInput = new EntityModelEditorInput(id, Entity.getEntityType(entityModel));
-                try {
-                    logger.log(new Status(Status.INFO, Activator.PLUGIN_ID, Status.OK, entityModelEditorInput.toString(), null));
-                    page.openEditor(entityModelEditorInput, EntityModelEditor.ID);
-                } catch (PartInitException ex) {
-                    logger.log(
-                            new Status(Status.ERROR, Activator.PLUGIN_ID, Status.ERROR, "An exception has occured when opening the editor", ex));
-                }
-            }
-        }
-    }
+			if (Entity.COMMENT == Entity.getEntityType(entityModel)) {
+				entityModel = (EntityModel) Util.getContainerItemForCommentModel(entityModel).getValue();
+			}
+
+			Long id = Long.parseLong(entityModel.getValue("id").getValue().toString());
+			EntityModelEditorInput entityModelEditorInput = new EntityModelEditorInput(id, Entity.getEntityType(entityModel));
+			try {
+				logger.log(new Status(Status.INFO, Activator.PLUGIN_ID, Status.OK, entityModelEditorInput.toString(), null));
+				page.openEditor(entityModelEditorInput, EntityModelEditor.ID);
+			} catch (PartInitException ex) {
+				logger.log(
+						new Status(Status.ERROR, Activator.PLUGIN_ID, Status.ERROR, "An exception has occured when opening the editor", ex));
+			}
+		}
+	}
 
 }
