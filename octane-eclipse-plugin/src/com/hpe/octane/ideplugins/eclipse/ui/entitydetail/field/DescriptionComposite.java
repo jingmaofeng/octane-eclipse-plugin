fdb5921dcbd6ebe20723b7e9efa78bf4e22f29c0
vladbenchea <vlad.benchea@hpe.com>
2018-04-20 11:45:53 +0300

    made another branch for the images in description because of conflicts

--- octane-eclipse-plugin/src/com/hpe/octane/ideplugins/eclipse/ui/entitydetail/field/DescriptionComposite.java
+++ octane-eclipse-plugin/src/com/hpe/octane/ideplugins/eclipse/ui/entitydetail/field/DescriptionComposite.java
@@ -12,14 +12,37 @@
  ******************************************************************************/
 package com.hpe.octane.ideplugins.eclipse.ui.entitydetail.field;
 
+import java.util.List;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpCookie;
+import java.util.Optional;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import com.google.api.client.http.ByteArrayContent;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.util.IOUtils;
+import com.hpe.adm.nga.sdk.authentication.SimpleUserAuthentication;
 import com.hpe.adm.nga.sdk.model.EntityModel;
 import com.hpe.adm.octane.ideplugins.services.util.Util;
+import com.hpe.octane.ideplugins.eclipse.Activator;
 import com.hpe.octane.ideplugins.eclipse.ui.entitydetail.model.EntityModelWrapper;
 import com.hpe.octane.ideplugins.eclipse.ui.util.LinkInterceptListener;
 import com.hpe.octane.ideplugins.eclipse.ui.util.PropagateScrollBrowserFactory;
@@ -33,6 +56,7 @@ public class DescriptionComposite extends Composite {
     private Color foregroundColor = PlatformResourcesManager.getPlatformForegroundColor();
     private Color backgroundColor = PlatformResourcesManager.getPlatformBackgroundColor();
     private Browser browserDescHtml;
+    private File octanePhotosDir;
 
     public DescriptionComposite(Composite parent, int style) {
         super(parent, style);
@@ -47,10 +71,17 @@ public class DescriptionComposite extends Composite {
         browserDescHtml.setText(getBrowserText(entityModelWrapper.getReadOnlyEntityModel()));
     }
 
-    private String getBrowserText(EntityModel entityModel) {
+    public String getBrowserText(EntityModel entityModel) {
+        String descriptionFromServerRemodeled = Util.getUiDataFromModel(entityModel.getValue((EntityFieldsConstants.FIELD_DESCRIPTION)));
+        try {
+            descriptionFromServerRemodeled = downloadPictures(
+                    Util.getUiDataFromModel(entityModel.getValue((EntityFieldsConstants.FIELD_DESCRIPTION))));
+        } catch (IOException e) {
+        }
+
         String descriptionText = "<html><body bgcolor =" + getRgbString(backgroundColor) + ">" + "<font color ="
                 + getRgbString(foregroundColor) + ">"
-                + Util.getUiDataFromModel(entityModel.getValue(EntityFieldsConstants.FIELD_DESCRIPTION))
+                + descriptionFromServerRemodeled
                 + "</font></body></html>";
         if (descriptionText.equals("<html><body bgcolor =" + getRgbString(backgroundColor) + ">" + "<font color ="
                 + getRgbString(foregroundColor) + ">" + "</font></body></html>")) {
@@ -60,10 +91,89 @@ public class DescriptionComposite extends Composite {
         } else {
             return descriptionText;
         }
+
     }
 
     private static String getRgbString(Color color) {
         return "rgb(" + color.getRed() + "," + color.getGreen() + "," + color.getBlue() + ")";
+    }
+    
+    private String downloadPictures(String descriptionField) throws IOException {
+        String tmpPath = System.getProperty("java.io.tmpdir");
+        String baseUrl = Activator.getConnectionSettings().getBaseUrl();
+        File tmpDir = new File(tmpPath);
+        octanePhotosDir = new File(tmpDir, "Octane_pictures");
+
+        if (!new File(tmpDir, "Octane_pictures").exists()) {
+            octanePhotosDir.mkdir();
+        }
+        String octanePhotosPath = octanePhotosDir.getAbsolutePath();
+
+        Document descriptionParser = Jsoup.parse(descriptionField);
+        Elements link = descriptionParser.getElementsByTag("img");
+        
+        for (Element el : link) {
+            String pictureLink = el.attr("src");
+            if (pictureLink.contains("/api/shared_spaces")) {
+                el.attr("src", baseUrl + pictureLink);
+                pictureLink = el.attr("src");
+            }
+
+            if (!pictureLink.contains(Activator.getConnectionSettings().getBaseUrl())) {
+                continue;
+            }
+            int index = pictureLink.lastIndexOf("/");
+            String pictureName = pictureLink.substring(index + 1, pictureLink.length());
+            String picturePath = octanePhotosPath + "\\" + pictureName;
+            if (!new File(octanePhotosDir, pictureName).exists()) {
+                saveImage(pictureLink, picturePath);
+            }
+            ;
+            el.attr("src", picturePath);
+        }
+        return descriptionParser.toString();
+    }
+
+    private void saveImage(String pictureLink, String octanePhotosName) throws IOException {
+
+        SimpleUserAuthentication userAuthentication = new SimpleUserAuthentication(
+                Activator.getConnectionSettings().getUserName(),
+                Activator.getConnectionSettings().getPassword());
+
+        HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+        HttpRequestFactory requestFactory = HTTP_TRANSPORT.createRequestFactory();
+
+        ByteArrayContent content = ByteArrayContent.fromString("application/json", userAuthentication.getAuthenticationString());
+        HttpRequest httpRequest = requestFactory
+                .buildPostRequest(new GenericUrl(Activator.getConnectionSettings().getBaseUrl() + "/authentication/sign_in"), content);
+
+        HttpResponse httpResponse = httpRequest.execute();
+
+        List<String> strHPSSOCookieCsrf1 = httpResponse.getHeaders().getHeaderStringValues("Set-Cookie");
+
+        HttpCookie lwssoCookie = null;
+
+        for (String strCookie : strHPSSOCookieCsrf1) {
+            List<HttpCookie> cookies = HttpCookie.parse(strCookie);
+            Optional<HttpCookie> lwssoCookieOp = cookies.stream().filter(a -> a.getName().equals("LWSSO_COOKIE_KEY")).findFirst();
+            if (lwssoCookieOp.isPresent()) {
+                lwssoCookie = lwssoCookieOp.get();
+                break;
+            } else {
+                return;
+            }
+        }
+
+        httpRequest = requestFactory.buildGetRequest(new GenericUrl(pictureLink));
+        httpRequest.getHeaders().setCookie(lwssoCookie.toString());
+        httpResponse = httpRequest.execute();
+
+        InputStream is = httpResponse.getContent();
+
+        OutputStream os = new FileOutputStream(octanePhotosName);
+        IOUtils.copy(is, os);
+        is.close();
+        os.close();
     }
 
 }
